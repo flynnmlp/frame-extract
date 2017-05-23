8345c1182a8ac9d36972eb73169b9e206b751fe9
Flynn <flynnmlp@gmail.com>
2017-05-24 00:02:45 +0200

    use github pages

--- build.sh
+++ build.sh
@@ -6,16 +6,7 @@ cd "$(dirname "$0")"
 
 mkdir -p dist
 
+cp node_modules/file-saver/FileSaver.min.js index.html style.css script.js dist/
+
 cat worker.js node_modules/jszip/dist/jszip.min.js > dist/worker.js
-
-(
-	echo -n "URL.createObjectURL(new Blob([atob(\""
-	base64 -w 0 dist/worker.js
-	echo -n "\")], {type: 'text/javascript'}))"
-)>dist/worker.base64
-
-cp node_modules/file-saver/FileSaver.min.js index.htm style.css script.js dist/
-
-
-awk 'BEGIN{getline l < "dist/worker.base64"}/Worker/{gsub("\"worker.js\"",l)}1' script.js >dist/script.js
 
